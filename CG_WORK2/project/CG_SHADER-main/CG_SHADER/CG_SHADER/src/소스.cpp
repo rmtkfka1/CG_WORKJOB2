ecb54e0ba004c35a43becef2f038f088705a9adc
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2023-11-25 01:36:23 +0900

    사운드 추가

--- CG_WORK2/project/CG_SHADER-main/CG_SHADER/CG_SHADER/src/소스.cpp
+++ CG_WORK2/project/CG_SHADER-main/CG_SHADER/CG_SHADER/src/소스.cpp
@@ -1,8 +1,7 @@
 #include "pch.h"
 #include "SceneManager.h"
-
-
-
+#include "fmod.hpp"
+#include "fmod_errors.h"
 
 void draw()
 {
@@ -19,8 +18,6 @@ void draw()
 int main(int argc, char** argv)
 {
 
-
-
 	Core::GetInstance()->Init(argc, argv);
 	Core::GetInstance()->Render(draw);
 
@@ -30,11 +27,23 @@ int main(int argc, char** argv)
 	MouseManager::GetInstance()->Init();
 
 
-
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glutSetCursor(GLUT_CURSOR_NONE); // 마우스 커서 숨기기
 
+	FMOD::System* ssystem;
+	FMOD::Sound* sound1, * sound2;
+	FMOD::Channel* channel = 0;
+	FMOD_RESULT result;
+	void* extradriverdata = 0;
+	
+	result = FMOD::System_Create(&ssystem); //--- 사운드 시스템 생성
+	if (result != FMOD_OK)
+		exit(0);
+
+	ssystem->init(32, FMOD_INIT_NORMAL, extradriverdata); //--- 사운드 시스템 초기화
+	ssystem->createSound("sound.mp3", FMOD_LOOP_NORMAL, 0, &sound1);
+	ssystem->playSound(sound1, 0, false, &channel);
 
 	SceneManager::GetInstance()->ChangeScene(SceneType::Stage1);
 
